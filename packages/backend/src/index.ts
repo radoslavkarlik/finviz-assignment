5bfa3d6a05911693ed1fd8af5ed4ed81689f3374
Buckyx <karlik.radoslav@gmail.com>
2026-03-24 19:13:29 +0100

    Add simple swagger UI

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import { readFileSync } from "fs";
 import { join } from "path";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
 const data: Array<unknown> = JSON.parse(readFileSync(join(__dirname, "parsed.json"), "utf-8"));
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   const page = parseInt((req.query.page as string) ?? "1", 10);
   const offset = parseInt((req.query.offset as string) ?? "20", 10);
 
@@ -16,6 +17,12 @@ app.get("/", (req, res) => {
 
   res.json(items);
 });
+
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(JSON.parse(readFileSync(join(__dirname, "swagger.json"), "utf-8"))),
+);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
